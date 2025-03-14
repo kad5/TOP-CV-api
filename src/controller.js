3d96b91425371c0efa184c0c7e74fcf127b03a0d
kad5 <khaled.mohamed.1807@gmail.com>
2025-03-14 19:38:37 +0200

    cookie fix

--- src/controller.js
+++ src/controller.js
@@ -13,17 +13,17 @@ const signup = asyncHandler(async (req, res) => {
 
   const hashedPassword = await argon2.hash(password);
   const user = await queries.createNewUser(username, hashedPassword);
-  console.log("sign up");
-  console.log("user:");
-  console.log(user);
+
   const accessToken = generateAccessToken(user.id);
   const refreshToken = generateRefreshToken(user.id);
   await queries.addRefreshToken(user.id, refreshToken);
-  console.log("refreshtoken added");
+
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/",
   });
   return res
     .status(201)
@@ -39,21 +39,18 @@ const login = asyncHandler(async (req, res) => {
   const isMatch = await argon2.verify(user.password, password);
   if (!isMatch)
     return res.status(401).json({ message: "invalid username or password" });
-  console.log("sign up");
-  console.log("user:");
-  console.log(user);
+
   const accessToken = generateAccessToken(user.id);
   const refreshToken = generateRefreshToken(user.id);
-  console.log("accessToken:");
-  console.log(accessToken);
-  console.log("refreshToken:");
-  console.log(refreshToken);
+
   await queries.addRefreshToken(user.id, refreshToken);
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/",
   });
   return res
     .status(200)
@@ -64,12 +61,13 @@ const logout = asyncHandler(async (req, res) => {
   const { refreshToken } = req.cookies;
   if (!refreshToken)
     return res.status(400).json({ message: "No token provided" });
-  console.log(refreshToken);
+
   await queries.deleteRefreshToken(refreshToken);
   res.clearCookie("refreshToken", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
+    path: "/",
   });
   return res.status(200).json({ message: "Logged out successfully" });
 });
@@ -88,13 +86,13 @@ const refreshToken = asyncHandler(async (req, res) => {
 
     const newAccessToken = generateAccessToken(decoded.id);
     const newRefreshToken = generateRefreshToken(decoded.id);
-
     await queries.updateRefreshToken(refreshToken, newRefreshToken);
-    console.log("updated");
     res.cookie("refreshToken", newRefreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      path: "/",
     });
     return res.status(200).json({ accessToken: newAccessToken });
   } catch (error) {
